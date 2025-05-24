e53613e781c3bbb0a388b22cd82dd9b57b67dd2a
EmirCoban <125053917+EmirCobanOfficial@users.noreply.github.com>
2025-05-24 18:18:09 +0300

    Update app.js

--- MusicPlayer/app.js
+++ MusicPlayer/app.js
@@ -19,6 +19,7 @@ window.addEventListener("load", () => {
     const music = player.getMusic();
     displayMusic(music);
     displayMusicList(player.musicList);
+    isPlayingNow();
 });
 
 function displayMusic(music) {
@@ -42,6 +43,7 @@ const nextMusic = () => {
     let music = player.getMusic();
     displayMusic(music);
     playMusic();
+    isPlayingNow();
 }
 
 const prevMusic = () => {
@@ -49,6 +51,7 @@ const prevMusic = () => {
     let music = player.getMusic();
     displayMusic(music);
     playMusic();
+    isPlayingNow();
 }
 
 
@@ -119,7 +122,7 @@ progressBar.addEventListener("input", () => {
 
 const displayMusicList = (list) => {
     for (let i = 0; i < list.length; i++) {
-        let liTag = `<li
+        let liTag = `<li li-index="${i}" onclick="selectedMusic(this)"
               class="list-group-items d-flex justify-content-between align-items-center">
               <span>${list[i].getName()}</span>
               <span id="music-${i}" class="badge bg-primary rounded-pill"></span>
@@ -136,3 +139,25 @@ const displayMusicList = (list) => {
         });
     }
 }
+
+const selectedMusic = (li) => {
+    player.index = li.getAttribute("li-index");
+    displayMusic(player.getMusic());
+    playMusic();
+    isPlayingNow();
+}
+
+const isPlayingNow = () => {
+    for (let li of ul.querySelectorAll("li")) {
+        if (li.classList.contains("playing")) {
+            li.classList.remove("playing");
+        }
+        if (li.getAttribute("li-index") == player.index) {
+            li.classList.add("playing");
+        }
+    }
+}
+
+audio.addEventListener("ended", () => {
+    nextMusic();
+});
